e8c0b13dc109b32aeb55632f7f42a11238031400
asis-parajuli <60426026+asis-parajuli@users.noreply.github.com>
2020-04-27 10:24:35 +0545

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDSuXHQQD4H0eREUhZjaSrx5xRFxSVysAA",
-  authDomain: "newser-76dd3.firebaseapp.com",
-  databaseURL: "https://newser-76dd3.firebaseio.com",
-  projectId: "newser-76dd3",
-  storageBucket: "newser-76dd3.appspot.com",
-  messagingSenderId: "661264800781",
-  appId: "1:661264800781:web:ac877779c3478567dac944",
+  //your firebase configuration
 };
 
 export default firebaseConfig;
